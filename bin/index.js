61c31314812437211a47e5bc39920b9c33decde2
Britton Walker <bwalker1801@gmail.com>
2025-03-05 17:16:11 -0500

    Update

--- bin/index.js
+++ bin/index.js
@@ -10,7 +10,19 @@ program
   .description('A commandline interface for installing the TKTK theme')
   .option('-v, --version', 'show version', ver, '')
   .action((file, options) => {
-    console.log('file name: ', file);
+    console.log('usage: tktk-installer [-v|--version] <command> [<args>]');
+    console.log('');
+    console.log('Commands:');
+    console.log('  install');
+    console.log('');
+    console.log('Options:');
+    console.log('  -v, --version  output the version number');
+    console.log('');
+    console.log('Examples:');
+    console.log('  $ tktk-installer install');
+    console.log('');
+    console.log('  $ tktk-installer -v');
+    console.log('');
   });
 
 program
