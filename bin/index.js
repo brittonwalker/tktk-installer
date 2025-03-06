a0c7a0cc86515194504e3ae0a4f72e29b6b9d7e8
Britton Walker <bwalker1801@gmail.com>
2025-03-06 18:35:20 -0500

    Leverage commander for commands

--- bin/index.js
+++ bin/index.js
@@ -1,36 +1,18 @@
 #!/usr/bin/env node
 
-const program = require('commander');
-
-const install = require('../lib/install');
-const ver = require('../lib/ver');
+const { program } = require('commander');
+const packageJson = require('../package.json');
+const install = require('../lib/install'); // Import install action
 
 program
-  .usage('[options] <file>')
-  .description('A commandline interface for installing the TKTK theme')
-  .option('-v, --version', 'show version', ver, '')
-  .action((file, options) => {
-    console.log('usage: tktk-installer [-v|--version] <command> [<args>]');
-    console.log('');
-    console.log('Commands:');
-    console.log('  install');
-    console.log('');
-    console.log('Options:');
-    console.log('  -v, --version  output the version number');
-    console.log('');
-    console.log('Examples:');
-    console.log('  $ tktk-installer install');
-    console.log('');
-    console.log('  $ tktk-installer -v');
-    console.log('');
-  });
+  .name('tktk-installer')
+  .description('CLI tool for installing the TKTK WordPress theme')
+  .version(packageJson.version, '-v, --version', 'Show version');
 
 program
   .command('install')
   .alias('i')
-  .description('Install the TKTK theme in a local directory')
-  .action(async () => {
-    install();
-  });
+  .description('Install the TKTK theme')
+  .action(install); // This executes the `install.js` function
 
 program.parse(process.argv);
